c2612386949b4ccfda0523be6406d9c695fc910a
けーすけ <>
2021-05-05 15:54:46 +0900

    implement reducer

---
+++ src/reducers/count.js
@@ -0,0 +1,16 @@
+// /action/index.jsで作成したactionのtypeをインポート
+import { INCREMENT, DECREMENT } from '../actions'
+
+// カウンターの初期値を設定
+const initialState = { value: 0 }
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+        return { value: state.value + 1 }
+    case DECREMENT:
+        return { value: state.value - 1 }
+    default: 
+        return state
+  }
+}
